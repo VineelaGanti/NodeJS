fbf224309c7b423c44e9814f92599a3429867241
vineela_Ganti <vineela.ganti@gmail.com>
2016-01-22 11:45:48 -0500

    Posts page

--- MEAN/app.js
+++ MEAN/app.js
@@ -20,7 +20,12 @@ app.config([
                 url: '/home',
                 templateUrl: '/home.html',
                 controller: 'MainCtrl'
-            });
+            })
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
+        });
 
         $urlRouterProvider.otherwise('home');
     }]);
@@ -45,8 +50,13 @@ app.controller('MainCtrl', [
             $scope.posts.push({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0
+                upvotes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
             });
+
             $scope.title = '';
             $scope.link = '';
         };
@@ -55,3 +65,19 @@ app.controller('MainCtrl', [
 
 ]);
 
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+        $scope.post = posts.posts[$stateParams.id];
+        $scope.addComment = function(){
+            if($scope.body === '') { return; }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
+    }]);
